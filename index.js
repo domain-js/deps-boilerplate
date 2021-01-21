fbfcd356cfbd60a6078782b1267b55bef3e24079
Redstone Zhao <13740080@qq.com>
2021-01-21 18:51:53 +0800

    project init

---
+++ index.js
@@ -0,0 +1,9 @@
+function Main(cnf, deps) {
+  // 这一行代码是为了避免eslint错误, 真实项目不需要保留
+  console.warn("After hook: %s", Object.keys(deps));
+  // TODO your code
+}
+
+Main.Deps = []; // e.g ["logger", "utils"];
+
+module.exports = Main;
