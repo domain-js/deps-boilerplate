3fbf95dba5aa63e6a875b54b291617ca4f6b1a85
Redstone Zhao <13740080@qq.com>
2021-01-24 08:04:58 +0800

    调整文档和注释

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function Main(cnf, deps) {
   // 这一行代码是为了避免eslint错误, 真实项目不需要保留
-  console.warn("After hook: %s", Object.keys(deps));
+  console.warn("Main function: deps.keys is %s", Object.keys(deps));
   // TODO your code
 }
 
